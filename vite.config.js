ae080478238319771d6991049dbeafed256eecfb
Jonna Barvsten <jonna.barvsten@gmail.com>
2026-03-22 19:09:46 +0100

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Labb_2_Dynamisk_Hemsida/'
+  base: '/labb-2-dynamisk-hemsida/'
 })
